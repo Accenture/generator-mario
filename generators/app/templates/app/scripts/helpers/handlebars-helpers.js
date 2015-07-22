5f8ce82ab8f187c3b1e0267a95b2ae163a777347
Martin Lofaj <martin.lofaj@accenture.com>
2015-07-22 10:52:37 +0200

    Added formatDay helper to app & crud
    Summary: added simple helper to format date object into day in a week (no localization)
    
    Test Plan: generate grud app, check detail and creaate view if contain day name
    
    Reviewers: #aowp-ba, slavomir.kubacka
    
    Reviewed By: #aowp-ba, slavomir.kubacka
    
    Subscribers: slavomir.kubacka
    
    JIRA Issues: larc-649
    
    Differential Revision: http://23.23.125.207/D745

--- generators/app/templates/app/scripts/helpers/handlebars-helpers.js
+++ generators/app/templates/app/scripts/helpers/handlebars-helpers.js
@@ -4,12 +4,24 @@ define(['handlebars'], function (Handlebars) {
   return {
     initialize: function () {
       Handlebars.registerHelper('formatDate', function (date) {
-          if (date !== undefined) {
+          if (date) {
               var d = new Date(date);
               return d.toLocaleTimeString() + ' - ' + d.toLocaleDateString();
           } else {
             return 'N/A';
           }
+      });
+
+      Handlebars.registerHelper('formatDay', function(date) {
+        var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        var dayName = '';
+
+        if(date) {
+          var d = new Date(date);
+          dayName = dayNames[d.getDay()];
+        }
+
+        return dayName;
       });
     }
   };
