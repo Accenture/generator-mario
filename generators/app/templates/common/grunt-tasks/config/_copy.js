a704f7d79bd5139686745e1e3837c83bc0c0c124
Martin Lofaj <martin.lofaj@accenture.com>
2015-12-15 15:05:31 +0100

    fixed icon fonts in sass option
    - updated imagemin
    - bumped minor version

--- generators/app/templates/common/grunt-tasks/config/_copy.js
+++ generators/app/templates/common/grunt-tasks/config/_copy.js
@@ -7,14 +7,16 @@ module.exports = function(grunt) {
 			files: [{
 				expand: true,
 				dot: true,
-				cwd: '<%= yeoman.app %>',
-				dest: '<%= yeoman.dist %>',
+				cwd: '<%%= yeoman.app %>',
+				dest: '<%%= yeoman.dist %>',
 				src: [
 					'*.{ico,txt}',
 					'.htaccess',
 					'jsondata/*.*',
 					'images/*.*',
-					'bower_components/font-awesome/fonts/{,*/}*.*',
+					'bower_components/font-awesome/fonts/{,*/}*.*',<% if(styles === 'less') { %>
+					'bower_components/bootstrap/fonts/{,*/}*.*',<% } else { %>
+					'bower_components/bootstrap-sass/assets/fonts/bootstrap/{,*/}*.*',<% } %>
 					'bower_components/modernizr/modernizr.js',
           'locales/**/*'
 				]
